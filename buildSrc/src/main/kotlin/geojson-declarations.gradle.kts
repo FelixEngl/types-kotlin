8734c88c805eb766e3862561c7a0694c2b568e5b
Felix Engl <felix.engl@uni-bamberg.de>
2022-06-14 17:48:53 +0200

    Add geojson
    Signed-off-by: Felix Engl <felix.engl@uni-bamberg.de>

--- buildSrc/src/main/kotlin/geojson-declarations.gradle.kts
+++ buildSrc/src/main/kotlin/geojson-declarations.gradle.kts
@@ -3,17 +3,16 @@ plugins {
 }
 
 tasks.named("generateDeclarations") {
-//    doLast {
-//        val definitionsFile = rootProject.buildDir
-//            .resolve("js/node_modules/@types/geojson/index.d.ts")
-//        val sourceDir = projectDir.resolve("src/main/kotlin")
-//
-//        delete(sourceDir)
-//
-//        karakum.geojson.generateKotlinDeclarations(
-//            definitionsDir = definitionsDir,
-//            typesDir = typesDir,
-//            sourceDir = sourceDir,
-//        )
-//    }
+    doLast {
+        val definitionsFile = rootProject.buildDir
+            .resolve("js/node_modules/@types/geojson/index.d.ts")
+        val sourceDir = projectDir.resolve("src/main/kotlin")
+
+        delete(sourceDir)
+
+        karakum.geojson.generate(
+            definitionsFile = definitionsFile,
+            sourceDir = sourceDir,
+        )
+    }
 }
---
+++ buildSrc/src/main/kotlin/karakum/geojson/Generator.kt
@@ -0,0 +1,10 @@
+package karakum.geojson
+
+import java.io.File
+
+fun generate(
+    definitionsFile: File,
+    sourceDir: File,
+) {
+
+}
